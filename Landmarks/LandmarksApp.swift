757f45f24fadf018720d0dbfb855d21dca08670e
ChiKim22 <wlsgh2505@gmail.com>
2021-12-28 19:33:24 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jinho Kim on 2021/12/28.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
