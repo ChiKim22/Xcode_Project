f0ccc5dfa5f4cb663f5c3e6a5f5eabebcb04e308
ChiKim22 <wlsgh2505@gmail.com>
2022-01-04 20:05:37 +0900

    SwiftUI Test

---
+++ SimpleClock/SimpleClock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleClock
+//
+//  Created by Jinho Kim on 2021/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleClock/SimpleClock/SimpleClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleClockApp.swift
+//  SimpleClock
+//
+//  Created by Jinho Kim on 2021/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
