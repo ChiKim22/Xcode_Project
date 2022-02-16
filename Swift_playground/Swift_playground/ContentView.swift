12a18bdb1c4469f213b4d7f2c4e38f0ea04241c7
ChiKim22 <wlsgh2505@gmail.com>
2022-02-16 16:33:07 +0900

    Xcode Project

---
+++ Swift_playground/Swift_playground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift_playground
+//
+//  Created by Jinho Kim on 2022/02/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift_playground/Swift_playground/Swift_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_playgroundApp.swift
+//  Swift_playground
+//
+//  Created by Jinho Kim on 2022/02/15.
+//
+
+import SwiftUI
+
+@main
+struct Swift_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
