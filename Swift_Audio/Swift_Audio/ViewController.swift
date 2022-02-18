062c941e1fe6d1d6c47a11753d18bf161804fed2
ChiKim22 <wlsgh2505@gmail.com>
2022-02-18 19:40:45 +0900

    Xcode Project

---
+++ Swift_Audio/Swift_Audio/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Swift_Audio
+//
+//  Created by Jinho Kim on 2022/02/16.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var audioPlayer: AVAudioPlayer?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
